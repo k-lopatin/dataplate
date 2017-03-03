d315862d23124d65ea95856880ce293b20b490c6
Konstantin Lopatin <k.lopatin@innopolis.ru>
2017-03-03 23:56:18 +0300

    Changed install to ubuntu 14.04 compatability

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ source functions.sh
 
 # Install all needed dependencies
 sudo apt-get install python3
-sudo apt-get install pip3
+sudo apt-get install python3-pip
 sudo apt-get install rabbitmq-server
-sudo apt-get install pylint3
+sudo pip3 install pylint3
 
 # Run all install scripts of submodules
 run_for_components ./install.sh
